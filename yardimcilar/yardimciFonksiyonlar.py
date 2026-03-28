b3bba6f078a47c045d2d08de9418a52661364b48
Mehmet Can Çalışkan <caliskanmehmetc@gmail.com>
2026-03-28 11:46:43 +0300

    nsga2 eklenmesi yapıldı

--- yardimcilar/yardimciFonksiyonlar.py
+++ yardimcilar/yardimciFonksiyonlar.py
@@ -142,3 +142,82 @@ def calculate_fitness_design(birey, aircraft):
     puan -= (toplam_cg_hatasi / len(aircraft.doluluk_oranlari)) * 1000
 
     return puan, dolu_cg_coords
+
+def calculate_fitness_nsga2(birey, aircraft):
+    """
+    NSGA-II için iki ayrı obje/hedef (objective) döndürür:
+    1. Ceza Puanı (Minimize edilecek): Çakışma, Taşma ve Titreşim cezaları
+    2. CG Hatası (Minimize edilecek): Ağırlık merkezinin hedeften sapması
+    """
+    ceza_puani = 0.0
+
+    # Çakışma Kontrolü
+    cakisma_sayisi = 0
+    keys = list(birey.yerlesim.keys())
+    for i in range(len(keys)):
+        for j in range(i+1, len(keys)):
+            k1_id = keys[i]
+            k2_id = keys[j]
+            dim1 = next(item for item in aircraft.komponentler_db if item.id == k1_id).boyut
+            dim2 = next(item for item in aircraft.komponentler_db if item.id == k2_id).boyut
+            pos1 = birey.yerlesim[k1_id]
+            pos2 = birey.yerlesim[k2_id]
+            if kutular_cakisiyor_mu(pos1, dim1, pos2, dim2):
+                cakisma_sayisi += 1
+                
+    ceza_puani += cakisma_sayisi * 10000
+
+    # Gövdeden Taşma Kontrolü
+    tasma_sayisi = 0
+    for k_id, pos in birey.yerlesim.items():
+        dim = next(item for item in aircraft.komponentler_db if item.id == k_id).boyut
+        if not aircraft.govde_icinde_mi(pos, dim):
+            tasma_sayisi += 1
+            
+    ceza_puani += tasma_sayisi * 5000
+
+    # TİTREŞİM KONTROLÜ
+    pos_motor = birey.yerlesim.get("Motor")
+    if pos_motor:
+        for k_id, pos in birey.yerlesim.items():
+            parca_db = next(item for item in aircraft.komponentler_db if item.id == k_id)
+            if parca_db.titresim_hassasiyeti:
+                mesafe = ((pos[0]-pos_motor[0])**2 + (pos[1]-pos_motor[1])**2 + (pos[2]-pos_motor[2])**2)**0.5
+                if mesafe < aircraft.titresim_limiti:
+                    ihlâl = aircraft.titresim_limiti - mesafe
+                    ceza_puani += (ihlâl ** 2) * 50
+
+    # CG (Ağırlık Merkezi) Miktarı
+    toplam_cg_hatasi = 0
+    dolu_cg_coords = (0, 0, 0)
+
+    for doluluk in aircraft.doluluk_oranlari:
+        total_mass = 0
+        moment_x = 0
+        moment_y = 0
+        moment_z = 0
+        for k_id, pos in birey.yerlesim.items():
+            db_item = next(item for item in aircraft.komponentler_db if item.id == k_id)
+            mass = db_item.agirlik
+            if k_id == "Yakit_Tanki":
+                mass += aircraft.max_yakit_agirligi * doluluk
+            total_mass += mass
+            moment_x += mass * pos[0]
+            moment_y += mass * pos[1]
+            moment_z += mass * pos[2]
+
+        cg_x = moment_x / total_mass
+        cg_y = moment_y / total_mass
+        cg_z = moment_z / total_mass
+
+        if doluluk == 1.0:
+            dolu_cg_coords = (cg_x, cg_y, cg_z)
+
+        target_x_center = (aircraft.target_cg_x_min + aircraft.target_cg_x_max) / 2
+        dist_error = ((cg_x - target_x_center)**2 + (cg_y - aircraft.target_cg_y)**2 + (cg_z - aircraft.target_cg_z)**2)**0.5
+        toplam_cg_hatasi += dist_error
+
+    cg_hatasi = toplam_cg_hatasi / len(aircraft.doluluk_oranlari)
+
+    return ceza_puani, cg_hatasi, dolu_cg_coords
+
